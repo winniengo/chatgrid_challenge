3ceeb6aad9c95001f22cd43c278bafa7d5c2d590
Winnie Ngo <ngowinn@gmail.com>
2017-01-30 01:20:53 -0800

    make TableRow draggable

--- frontend/components/dialogs/table.jsx
+++ frontend/components/dialogs/table.jsx
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 import Modal from './modal';
 import TableRow from './table_row';
 
-export default (props) => (
-  <section>
-    <div className='dialog-table'>
-      {props.index.map((dialog, idx) => (
-        <TableRow
-          key={idx}
-          idx={idx}
-          dialog={dialog}
-          openModal={props.openModal}
-          editDialog={props.editDialog}
-          deleteDialog={props.deleteDialog} />
-      ))}
-    </div>
-    <button onClick={() => props.openModal(props.createDialog, 'add')}>Add</button>
-    <Modal
-      modalIsOpen={props.modalIsOpen}
-      closeModal={props.closeModal}
-      handleSubmit={props.modalHandleSubmit}
-      formType={props.modalFormType}
-      formInput={props.modalFormInput}
-    />
-  </section>
-);
+class Table extends Component {
+  render() {
+    return (
+      <section>
+        <div className='dialog-table'>
+          {this.props.index.map((dialog, index) => (
+            <TableRow
+              key={idx}
+              index={index}
+              dialog={dialog}
+              openModal={this.props.openModal}
+              editDialog={this.props.editDialog}
+              deleteDialog={this.props.deleteDialog} />
+          ))}
+        </div>
+        <button onClick={() => this.props.openModal(this.props.createDialog, 'add')}>Add</button>
+        <Modal
+          modalIsOpen={this.props.modalIsOpen}
+          closeModal={this.props.closeModal}
+          handleSubmit={this.props.modalHandleSubmit}
+          formType={this.props.modalFormType}
+          formInput={this.props.modalFormInput}
+        />
+      </section>
+    );
+  }
+}
+
+export default DragDropContext(HTML5Backend)(Table);
--- frontend/components/dialogs/table_row.jsx
+++ frontend/components/dialogs/table_row.jsx
@@ -1,14 +1,100 @@
-import React from 'react';
+// export default ({ idx, dialog, openModal, editDialog, deleteDialog }) => {
+//     const handleEdit = () => openModal(editDialog(idx), 'edit', dialog);
+//     const handleDelete = () => deleteDialog(idx);
+//
+//     return (
+//       <Row
+//         dialog={dialog}
+//         handleEdit={handleEdit}
+//         handleDelete={handleDelete} />
+//     );
+// }
+
+import React, { Component, PropTypes } from 'react';
+import { findDOMNode } from 'react-dom';
+import { DragSource, DropTarget } from 'react-dnd';
+import { merge } from 'lodash';
 import Row from './row';
 
-export default ({ idx, dialog, openModal, editDialog, deleteDialog }) => {
-    const handleEdit = () => openModal(editDialog(idx), 'edit', dialog);
-    const handleDelete = () => deleteDialog(idx);
+const style = {
+  border: '1px dashed gray',
+  padding: '0.5rem 1rem',
+  marginBottom: '.5rem',
+  backgroundColor: 'white',
+  cursor: 'move',
+};
 
-    return (
-      <Row
-        dialog={dialog}
-        handleEdit={handleEdit}
-        handleDelete={handleDelete} />
-    );
+const rowSource = {
+  beginDrag(props) {
+    return {
+      id: props.id,
+      index: props.index
+    };
+  }
+};
+
+const collectSource = (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging()
+});
+
+const rowTarget = {
+  hover(props, monitor, component) {
+    const dragIndex = monitor.getItem().index;
+    const hoverIndex = props.index;
+
+    if (dragIndex === hoverIndex) {
+      return; // don't replace dialogs with themselves
+    }
+
+    const hoverBoundingRect = findDOMNode(component).getBoundingClientRect();
+    const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+    const clientOffset = monitor.getClientOffset();
+    const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+
+    // only perform the move when the mouse has crossed half of the items height
+    if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+      return; // when dragging downwards, only move when the cursor is below 50%
+    }
+
+    if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+      return; // when dragging upwards, only move when the cursor is above 50%
+    }
+
+    props.moveRow(dragIndex, hoverIndex);
+
+    monitor.getItem().index = hoverIndex;
+  }
 }
+
+const collectTarget = connect => ({
+  connectDropTarget: connect.dropTarget(),
+});
+
+class TableRow extends Component {
+  render () {
+
+    const { index, dialog, openModal, editDialog, deleteDialog } = this.props;
+    const { isDragging, connectDragSource, connectDropTarget } = this.props;
+    const opacity = isDragging ? 0 : 1;
+
+    const handleEdit = () => openModal(editDialog(index), 'edit', dialog);
+    const handleDelete = () => deleteDialog(index);
+
+    return connectDropTarget(connectDragSource(
+      <div style={merge({}, style, { opacity })}>
+        <Row
+          dialog={dialog}
+          handleEdit={handleEdit}
+          handleDelete={handleDelete} />
+      </div>
+    ));
+  }
+}
+
+export default DropTarget(
+  'row',
+  rowTarget,
+  collectTarget
+)(DragSource('row', rowSource, collectSource)(TableRow));
